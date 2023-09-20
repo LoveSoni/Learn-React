23a6f520a8625a1f176429d8a9f42722ac5069c4
Love <lovesoni607@gmail.com>
2023-09-20 21:16:10 +0530

    starting from scratch

--- 01basic_react_app/src/practise.js
+++
@@ -1,6 +0,0 @@
-function Pract() {
-    return (
-        <h1> hey this one is from the practise</h1>
-    )
-}
-export default Pract;
--- 01vitereact/src/prac.jsx
+++
@@ -1,7 +0,0 @@
-function Practise() {
-    return (
-        <h1>Practising linking</h1>
-    )
-}
-
-export default Practise
