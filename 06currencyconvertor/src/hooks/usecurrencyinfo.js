654b31d55154380a1efa96ba9b7980dd0797e927
Love <lovesoni607@gmail.com>
2023-10-08 17:05:51 +0530

    added bootstrap to text_utils

---
+++ 06currencyconvertor/src/hooks/usecurrencyinfo.js
@@ -0,0 +1,12 @@
+import { useState, useEffect } from "react";
+
+function useCurrencyInfo(currency) {
+    useEffect(
+        () => {
+            fetch('https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${currency}.json')
+                .then((res) => res.json())
+                .then((res) => )
+                .catch(error => )
+        }, [currency]
+    )
+}
